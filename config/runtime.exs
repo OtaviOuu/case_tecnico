fd1b4a776563e4efabd023a2baf924a274bf04ce
otaviouu <otavio.venancio.oliveira@usp.br>
2026-03-17 23:25:54 -0300

    rmeove db

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,17 +24,6 @@ config :uhedge_case_tecnico, UhedgeCaseTecnicoWeb.Endpoint,
   http: [port: String.to_integer(System.get_env("PORT", "4000"))]
 
 if config_env() == :prod do
-  database_path =
-    System.get_env("DATABASE_PATH") ||
-      raise """
-      environment variable DATABASE_PATH is missing.
-      For example: /etc/uhedge_case_tecnico/uhedge_case_tecnico.db
-      """
-
-  config :uhedge_case_tecnico, UhedgeCaseTecnico.Repo,
-    database: database_path,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
