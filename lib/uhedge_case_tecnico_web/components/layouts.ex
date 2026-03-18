ef1d31762c10bda45b69ba99154646dbb910b876
otaviouu <otavio.venancio.oliveira@usp.br>
2026-03-17 21:10:19 -0300

    navbar + improve mobile ui

--- lib/uhedge_case_tecnico_web/components/layouts.ex
+++ lib/uhedge_case_tecnico_web/components/layouts.ex
@@ -35,32 +35,13 @@ defmodule UhedgeCaseTecnicoWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="navbar-start"></div>
+      <div class="navbar-center">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </header>
+      <div class="navbar-end"></div>
+    </div>
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-4xl space-y-4">
